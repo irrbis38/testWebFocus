a837c2cc64b66ecd3c75796d5ab3443fff22f0f2
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2020-11-04 14:09:17 +0300

    adaptive styles added

--- js/main.js
+++ js/main.js
@@ -1,19 +1,56 @@
 $(document).ready(function(){
 
-  $('.main__slider').slick({
-	  arrows: true,
-	  slidesToShow: 1,
-    speed: 1000,
-    prevArrow: $('.main__prev'),
-    nextArrow: $('.main__next')
-  });
+//burger menu
 
-  $('.principles__slider').slick({
-	  arrows: true,
-	  slidesToShow: 1,
-    speed: 1000,
-    prevArrow: $('.principles__prev'),
-    nextArrow: $('.principles__next')
-  });
-  
+	$('.header__burger').click(function(event) {
+			$('.header__burger, .header__menu').toggleClass('active');
+			$('body').toggleClass('lock');
+	});
+	$('.header__navLink').click(function(event) {
+
+			$('.header__burger,.header__menu').removeClass('active');
+
+			$('body').removeClass('lock');
+	});
+
+//first slider
+
+	$('.main__slider').slick({
+		arrows: true,
+		slidesToShow: 1,
+		speed: 1000,
+		prevArrow: $('.main__prev'),
+		nextArrow: $('.main__next'),
+		responsive: [
+			{
+			 breakpoint: 767,
+			 settings: {
+					slidesToShow: 1,
+					arrows: false,
+		 			dots: true
+			 }
+		 }
+		]
+	});
+
+//second slider
+
+	$('.principles__slider').slick({
+		arrows: true,
+		slidesToShow: 1,
+		speed: 1000,
+		prevArrow: $('.principles__prev'),
+		nextArrow: $('.principles__next'),
+		responsive: [
+			{
+			 breakpoint: 576,
+			 settings: {
+					slidesToShow: 1,
+					arrows: false,
+		 			dots: true
+			 }
+		 }
+		]
+	});
+	
 });
